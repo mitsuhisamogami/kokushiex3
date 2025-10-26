6abc10dfdb7787af4734401866d9cb033507b428
mogami <ndarfc.57@gmail.com>
2025-10-26 18:39:49 +0900

    [add]DashboardControllerとUserResponsesControllerに認証機能を追加

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -8,13 +8,22 @@ RSpec.describe 'Dashboards' do
     let!(:examination) { create(:examination, test:, user:) }
     let!(:score) { create(:score, examination:) }
 
-    before do
-      sign_in user
+    context '認証済みユーザーの場合' do
+      before do
+        sign_in user
+      end
+
+      it 'returns http success' do
+        get '/dashboard'
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'returns http success' do
-      get '/dashboard'
-      expect(response).to have_http_status(:success)
+    context '未認証ユーザーの場合' do
+      it 'ログインページにリダイレクトされる' do
+        get '/dashboard'
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
--- spec/requests/user_responses_spec.rb
+++ spec/requests/user_responses_spec.rb
@@ -14,31 +14,40 @@ RSpec.describe 'UserResponses' do
       }
     end
 
-    before do
-      sign_in user
+    context '認証済みユーザーの場合' do
+      before do
+        sign_in user
+      end
+
+      context '正常系' do
+        before do
+          allow(Examination).to receive(:create_result!)
+        end
+
+        it 'user_responseが作成されリダイレクトされる' do
+          post(user_responses_path, params:)
+          expect(response).to redirect_to(dashboard_path)
+          expect(flash[:notice]).to eq '試験結果を保存しました'
+        end
+      end
+
+      context '異常系' do
+        before do
+          allow(Examination).to receive(:create_result!).and_raise(StandardError)
+        end
+
+        it 'newテンプレートがレンダリングされ、エラーメッセージが表示される' do
+          post(user_responses_path, params:)
+          expect(response).to redirect_to(test_path(params[:test_id]))
+          expect(flash[:alert]).to eq '試験結果を保存できませんでした'
+        end
+      end
     end
 
-    context '正常系' do
-      before do
-        allow(Examination).to receive(:create_result!)
-      end
-
-      it 'user_responseが作成されリダイレクトされる' do
+    context '未認証ユーザーの場合' do
+      it 'ログインページにリダイレクトされる' do
         post(user_responses_path, params:)
-        expect(response).to redirect_to(dashboard_path)
-        expect(flash[:notice]).to eq '試験結果を保存しました'
-      end
-    end
-
-    context '異常系' do
-      before do
-        allow(Examination).to receive(:create_result!).and_raise(StandardError)
-      end
-
-      it 'newテンプレートがレンダリングされ、エラーメッセージが表示される' do
-        post(user_responses_path, params:)
-        expect(response).to redirect_to(test_path(params[:test_id]))
-        expect(flash[:alert]).to eq '試験結果を保存できませんでした'
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
