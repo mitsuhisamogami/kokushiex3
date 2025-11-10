fbd55e727e4fe320d58518347ca1ac7a97000781
mogami <ndarfc.57@gmail.com>
2025-11-11 00:23:12 +0900

    [add] ゲスト向けアップグレード案内カードを追加

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -2,15 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Accounts' do
   let(:user) { create(:user) }
-
-  before do
-    sign_in user
-  end
+  let(:guest_user) { create(:user, :guest) }
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/account'
-      expect(response).to have_http_status(:success)
+    context '通常ユーザー' do
+      before { sign_in user }
+
+      it 'returns http success' do
+        get '/account'
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'ゲスト向けカードは表示されない' do
+        get '/account'
+        expect(response.body).not_to include(I18n.t('guest_upgrade_card.title'))
+      end
+    end
+
+    context 'ゲストユーザー' do
+      before { sign_in guest_user }
+
+      it 'ゲスト向けカードが表示される' do
+        get '/account'
+        expect(response.body).to include(I18n.t('guest_upgrade_card.title'))
+      end
     end
   end
 end
