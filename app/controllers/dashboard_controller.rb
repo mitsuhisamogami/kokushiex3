a128ff97339248fcecbb29dd9e641f850da525f8
mogami <ndarfc.57@gmail.com>
2026-06-14 13:49:45 +0900

    [add] ダッシュボードに受験履歴テーブルを追加する

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,34 @@
 class DashboardController < ApplicationController
+  EXAMINATIONS_PER_PAGE = 10
+
   before_action :authenticate_user!
 
   def index
-    # current_userの最新のexamaminationを取得
-    @latest_examination = current_user.examinations.order(created_at: :desc).first
-    # latest_examinationのscoreを取得
-    @latest_score = @latest_examination.score if @latest_examination
+    examinations_scope = ordered_examinations
+    @total_examinations_count = examinations_scope.count
+    @total_history_pages = total_history_pages
+    @history_page = requested_page.clamp(1, @total_history_pages)
+    @latest_examination = examinations_scope.first
+    @latest_score = @latest_examination&.score
+    @examinations = examinations_scope
+                    .offset((@history_page - 1) * EXAMINATIONS_PER_PAGE)
+                    .limit(EXAMINATIONS_PER_PAGE)
+  end
+
+  private
+
+  def ordered_examinations
+    current_user
+      .examinations
+      .includes(:score, test: :pass_mark)
+      .order(attempt_date: :desc, created_at: :desc)
+  end
+
+  def requested_page
+    params[:page].to_i
+  end
+
+  def total_history_pages
+    [(@total_examinations_count.to_f / EXAMINATIONS_PER_PAGE).ceil, 1].max
   end
 end
